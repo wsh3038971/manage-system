ee5d2bc9a85e468693f32444989f35c059678717
王煜 <964934202@qq.com>
2020-09-20 21:47:53 +0800

    新建组件页面

---
+++ src/views/other/page2.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="page2">page2</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/videoManage/videoManage.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="videoManage">videoManage</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
